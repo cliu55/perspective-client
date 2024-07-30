ac30404cfa8487a659cc5f9864b2ad1e2151c053
Chris Liu <chrisliu@chriss-mbp.lan>
2024-07-29 22:20:29 -0500

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,55 @@
+// ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+// ┃ ██████ ██████ ██████       █      █      █      █      █ █▄  ▀███ █       ┃
+// ┃ ▄▄▄▄▄█ █▄▄▄▄▄ ▄▄▄▄▄█  ▀▀▀▀▀█▀▀▀▀▀ █ ▀▀▀▀▀█ ████████▌▐███ ███▄  ▀█ █ ▀▀▀▀▀ ┃
+// ┃ █▀▀▀▀▀ █▀▀▀▀▀ █▀██▀▀ ▄▄▄▄▄ █ ▄▄▄▄▄█ ▄▄▄▄▄█ ████████▌▐███ █████▄   █ ▄▄▄▄▄ ┃
+// ┃ █      ██████ █  ▀█▄       █ ██████      █      ███▌▐███ ███████▄ █       ┃
+// ┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫
+// ┃ Copyright (c) 2017, the Perspective Authors.                              ┃
+// ┃ ╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌ ┃
+// ┃ This file is part of the Perspective library, distributed under the terms ┃
+// ┃ of the [Apache License 2.0](https://www.apache.org/licenses/LICENSE-2.0). ┃
+// ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import perspective, { Table } from "@finos/perspective";
+
+import "@finos/perspective-viewer";
+import "@finos/perspective-viewer-datagrid";
+import "@finos/perspective-viewer-d3fc";
+import {
+  PerspectiveViewerConfig,
+  HTMLPerspectiveViewerElement,
+} from "@finos/perspective-viewer";
+
+import "./index.css";
+
+const worker = perspective.shared_worker();
+
+const getTable = async (): Promise<Table> => {
+  const websocket = perspective.websocket("ws://localhost:8000/websocket");
+  const table = await websocket.open_table("data_source_one");
+  const view = await table.view();
+  return await worker.table(view);
+};
+
+const config: PerspectiveViewerConfig = {};
+
+const App = (): React.ReactElement => {
+  const viewer = React.useRef<HTMLPerspectiveViewerElement>(null);
+
+  React.useEffect(() => {
+    getTable().then((table) => {
+      if (viewer.current) {
+        viewer.current.load(table);
+        viewer.current.restore(config);
+      }
+    });
+  }, []);
+
+  return <perspective-viewer ref={viewer}></perspective-viewer>;
+};
+
+window.addEventListener("load", () => {
+  ReactDOM.render(<App />, document.getElementById("root"));
+});
